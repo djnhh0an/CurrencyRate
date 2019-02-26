11ddbc38c03bec4f129b32447a72b98b29e29424
Hoan Tran <mr.dinhhoan@gmail.com>
2019-02-27 01:04:07 +0700

    Upload first demo source code
    Upload first demo source code

---
+++ CurrencyRate.ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrencyRate.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CurrencyRate.Infrastructures/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CurrencyRate.Infrastructures
+{
+    public class Class1
+    {
+    }
+}
